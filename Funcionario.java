862230447bf02cc6018f18152cb5057c969d27bd
Your Name <you@example.com>
2025-09-19 16:32:41 -0300

    Criei as minhas classes

---
+++ Funcionario.java
@@ -0,0 +1,22 @@
+public class Funcionario {
+    private int id;
+    private String nome;
+
+    public Funcionario(int id, String nome, ){
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public void cadastrarProduto(Produto p){
+
+    }
+    public Produto consultarProduto(int codigo){
+
+    }
+    public Venda gerarVenda(){
+
+    }
+    public Venda consultarVenda(int id){
+
+    }  
+}
---
+++ Venda.java
@@ -0,0 +1,5 @@
+public class Venda extends Funcionario {
+    private int id
+    private 
+    
+}
